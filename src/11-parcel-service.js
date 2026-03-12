5ab3965d6a51c93f0018deebec33b9027ad1168e
Faizan Ahmad <121797686+Faiz7112@users.noreply.github.com>
2026-03-12 14:16:46 +0530

    Completed 11

--- src/11-parcel-service.js
+++ src/11-parcel-service.js
@@ -52,21 +52,31 @@
  *   stringToChars("Dak")                  // => ["D", "a", "k"]
  */
 export function parcelToJSON(parcel) {
-  // Your code here
+  try {
+    return parcel === undefined ? "" : JSON.stringify(parcel);
+  } 
+  catch (error) {
+    return "";
+  }
 }
 
 export function jsonToParcel(jsonString) {
-  // Your code here
+  try {
+    return typeof jsonString !== "string" ? null : JSON.parse(jsonString);
+  }
+  catch (error) {
+    return null;
+  }
 }
 
 export function convertToString(value) {
-  // Your code here
+  return String(value);
 }
 
 export function convertToNumber(value) {
-  // Your code here
+  return Number(value);
 }
 
 export function stringToChars(str) {
-  // Your code here
+  return typeof str !== "string" ? [] : Array.from(str);
 }
